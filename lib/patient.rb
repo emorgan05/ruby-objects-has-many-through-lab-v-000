74c91fead35dbe61cd2885555a527dce8f51a3ed
Emily Morgan <emorgan05@gmail.com>
2017-10-02 15:41:58 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,4 +5,18 @@ class Patient
     @name = name
     @appointments = []
   end
+
+  def add_appointment(appointment)
+    @appointments << appointment
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def doctors
+    @appointments.collect do |appointment|
+      appointment.doctor
+    end
+  end
 end
